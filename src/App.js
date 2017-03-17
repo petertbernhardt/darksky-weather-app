886d74589e4734de11435075e52d1cc3cf5b8104
Bernhardt, Peter (HAL Contractor) <PBernhardt@HollandAmericaGroup.com>
2017-03-17 09:30:16 -0700

    Got fetchJsonp to console log data for seattle.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import fetchJsonp from 'fetch-jsonp';
 import './App.css';
 import {WeatherTile} from './components/weatherTile.js';
 import {WeatherNav} from './components/weatherNav.js';
+
+fetchJsonp('https://api.darksky.net/forecast/61df4ca5d6853174a5cb712cc3ef8fb4/47.6062,-122.3321')
+  .then(function(response) {
+    return response.json()
+  }).then(function(json) {
+    console.log('parsed json', json)
+  }).catch(function(ex) {
+    console.log('parsing failed', ex)
+  });
 
 class App extends Component {
   render() {
