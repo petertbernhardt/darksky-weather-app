2497a156f469ffba9f4f503b91bf3708622a644c
Bernhardt, Peter (HAL Contractor) <PBernhardt@HollandAmericaGroup.com>
2017-03-17 09:11:30 -0700

    merge from dev

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 import './App.css';
-
-class WeatherTile extends React.Component {
-  render() {
-    return <div className="weather-tile">{this.props.children}</div>;
-  }
-}
-
-class WeatherNav extends React.Component {
-  render() {
-    return <Navbar inverse>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a>Weather App</a>
-        </Navbar.Brand>
-      </Navbar.Header>
-    </Navbar>;
-  }
-}
+import {WeatherTile} from './components/weatherTile.js';
+import {WeatherNav} from './components/weatherNav.js';
 
 class App extends Component {
   render() {
---
+++ src/components/weatherNav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+export class WeatherNav extends React.Component {
+  render() {
+    return <Navbar inverse>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a>Weather App</a>
+        </Navbar.Brand>
+      </Navbar.Header>
+    </Navbar>;
+  }
+}
