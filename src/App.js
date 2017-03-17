48155eff35d486ebf300407a8a4c2a6b166466b5
Bernhardt, Peter (HAL Contractor) <PBernhardt@HollandAmericaGroup.com>
2017-03-17 10:27:40 -0700

    Got data to load into the weather tile. Currently showing seven tiles with the current weather and temperature.

--- src/App.js
+++ src/App.js
@@ -4,23 +4,42 @@ import './App.css';
 import {WeatherTile} from './components/weatherTile.js';
 import {WeatherNav} from './components/weatherNav.js';
 
-fetchJsonp('https://api.darksky.net/forecast/61df4ca5d6853174a5cb712cc3ef8fb4/47.6062,-122.3321')
-  .then(function(response) {
-    return response.json()
-  }).then(function(json) {
-    console.log('parsed json', json)
-  }).catch(function(ex) {
-    console.log('parsing failed', ex)
-  });
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { weatherData: {} };
+  }
+
+  getWeatherData() {
+    return fetchJsonp('https://api.darksky.net/forecast/61df4ca5d6853174a5cb712cc3ef8fb4/47.6062,-122.3321')
+      .then(function(response) {
+        return response.json();
+      });
+  }
+
+  componentDidMount() {
+    var data;
+    this.getWeatherData()
+      .then((json) => {
+        data = json;
+        this.setState({
+          weatherData: data
+        });
+        console.log(this.state.weatherData);
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <WeatherNav />
-        <WeatherTile>1</WeatherTile>
-        <WeatherTile>2</WeatherTile>
-        <WeatherTile>3</WeatherTile>
+        <WeatherTile>{this.state.weatherData}</WeatherTile>
+        <WeatherTile>{this.state.weatherData}</WeatherTile>
+        <WeatherTile>{this.state.weatherData}</WeatherTile>
+        <WeatherTile>{this.state.weatherData}</WeatherTile>
+        <WeatherTile>{this.state.weatherData}</WeatherTile>
+        <WeatherTile>{this.state.weatherData}</WeatherTile>
+        <WeatherTile>{this.state.weatherData}</WeatherTile>
       </div>
     );
   }
--- src/components/weatherTile.js
+++ src/components/weatherTile.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 export class WeatherTile extends React.Component {
   render() {
-    return <div className="weather-tile">{this.props.children}</div>;
+	return <div className="weather-tile">
+		{this.props.children.currently &&
+			"Weather: " + this.props.children.currently.summary +
+			" Temperature: " + this.props.children.currently.temperature
+	    }
+	</div>;
   }
 }
