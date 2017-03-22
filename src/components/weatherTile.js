a22b9820451c36217b545bec79afb0c1d185b86c
Bernhardt, Peter (HAL Contractor) <PBernhardt@HollandAmericaGroup.com>
2017-03-22 11:56:26 -0700

    Installed react-weathericons. Upgraded the look and feel for the weatherTile so it uses dynamic rendering.

--- src/components/weatherTile.js
+++ src/components/weatherTile.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import WeatherIcons from 'react-weathericons';
 
 export class WeatherTile extends React.Component {
   render() {
-	return <div className="weather-tile">
-		{!this.props.children.currently &&
-			"Loading data..."
-	    }
-		{this.props.children.currently &&
-			"Weather: " + this.props.children.currently.summary +
-			" Temperature: " + this.props.children.currently.temperature
-	    }
-	</div>;
+
+  	let contents = null;
+  	if (!this.props.children.currently) {
+  		contents = <p>Loading data...</p>;
+  	} else {
+  		let name = '';
+  		let currentWeather = this.props.children.currently.summary;
+  		let temperature = this.props.children.currently.temperature;
+  		let weatherIcon = '';
+  		switch(currentWeather) {
+  			case 'Mostly Cloudy':
+  				weatherIcon = 'day-cloudy-high';
+  			case 'Overcast':
+  				weatherIcon = 'day-cloudy';
+  		}
+  		contents =
+	  		<div>
+	  			<WeatherIcons name={weatherIcon} size='2x' />
+	  			<p>Weather:  { currentWeather }</p>
+	  			<p>Temperature: { temperature }</p>
+	  		</div>;
+  	}
+
+	return <div className="weather-tile">{ contents }</div>;
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import './css/weather-icons.css';
 import './index.css';
 
 ReactDOM.render(
