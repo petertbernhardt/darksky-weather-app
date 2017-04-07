affc32f194ae3cdaf0a414e03cc393f724f8c638
Bernhardt, Peter (HAL Contractor) <PBernhardt@HollandAmericaGroup.com>
2017-04-07 09:40:51 -0700

    Reduced redundancy in weatherTile.js.

--- src/components/weatherTile.js
+++ src/components/weatherTile.js
@@ -30,21 +30,17 @@ export class WeatherTile extends React.Component {
 			}
 		}
 
-		let contents = null;
-		// get the current time and set what time this weather tile is for
-		let currentDate = new Date();
-		if (this.props.hourly) {
-			if (!this.props.children.currently) {
-				contents = <p>Loading data...</p>;
-			} else {
-				let number = this.props.number;
+		function setUpWeatherTile(props, currentDate, isHourly, isDaily) {
+			if (isHourly) {
+				let number = props.number;
 				let time = (currentDate.getHours() + number) + ':' + currentDate.getMinutes();
 				// loop through the hours
-				let hourlyData = getHourlyData(number, this.props.children.hourly);
+				let hourlyData = getHourlyData(number, props.children.hourly);
 				let currentWeather = hourlyData.summary;
 				let temperature = hourlyData.temperature;
 				let weatherIcon = '';
 				weatherIcon = getWeatherIcon(currentWeather, hourlyData.icon);
+				// eslint-disable-next-line
 				contents =
 				<div>
 					<p>Time: { time }</p>
@@ -52,21 +48,18 @@ export class WeatherTile extends React.Component {
 					<p>Weather:  { currentWeather }</p>
 					<p>Temperature: { temperature }</p>
 				</div>;
-			}
-		} else if (this.props.daily) {
-			if (!this.props.children.currently) {
-				contents = <p>Loading data...</p>;
-			} else {
-				let number = this.props.number;
+			} else if (isDaily) {
+				let number = props.number;
 				let month = currentDate.getMonth() + 1;
 				let day = currentDate.getDate() + number;
 				// loop through the hours
-				let dailyData = getDailyData(number, this.props.children.daily);
+				let dailyData = getDailyData(number, props.children.daily);
 				let currentWeather = dailyData.summary;
 				let temperatureMin = dailyData.temperatureMin;
 				let temperatureMax = dailyData.temperatureMax;
 				let weatherIcon = '';
 				weatherIcon = getWeatherIcon(currentWeather);
+				// eslint-disable-next-line
 				contents =
 				<div>
 					<p>{ month }/{ day }</p>
@@ -75,18 +68,15 @@ export class WeatherTile extends React.Component {
 					<p>Temperature Min: { temperatureMin }</p>
 					<p>Temperature Max: { temperatureMax }</p>
 				</div>;
-			}
-		} else {
-			if (!this.props.children.currently) {
-				contents = <p>Loading data...</p>;
 			} else {
 				let month = currentDate.getMonth() + 1;
 				let day = currentDate.getDate();
 				let time = currentDate.getHours() + ':' + currentDate.getMinutes();
-				let currentWeather = this.props.children.currently.summary;
-				let temperature = this.props.children.currently.temperature;
+				let currentWeather = props.children.currently.summary;
+				let temperature = props.children.currently.temperature;
 				let weatherIcon = '';
 				weatherIcon = getWeatherIcon(currentWeather);
+				// eslint-disable-next-line
 				contents =
 				<div>
 					<p>{ month }/{ day }</p>
@@ -95,6 +85,29 @@ export class WeatherTile extends React.Component {
 					<p>Weather:  { currentWeather }</p>
 					<p>Temperature: { temperature }</p>
 				</div>;
+			}
+		}
+
+		let contents = null;
+		// get the current time and set what time this weather tile is for
+		let currentDate = new Date();
+		if (this.props.hourly) {
+			if (!this.props.children.currently) {
+				contents = <p>Loading data...</p>;
+			} else {
+				setUpWeatherTile(this.props, currentDate, true, false);
+			}
+		} else if (this.props.daily) {
+			if (!this.props.children.currently) {
+				contents = <p>Loading data...</p>;
+			} else {
+				setUpWeatherTile(this.props, currentDate, false, true);
+			}
+		} else {
+			if (!this.props.children.currently) {
+				contents = <p>Loading data...</p>;
+			} else {
+				setUpWeatherTile(this.props, currentDate, false, false);
 			}
 		}
 
